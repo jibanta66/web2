dad311e488eac89900d6b0b92390e60f553e2741
Jibanta <63874888+jibanta66@users.noreply.github.com>
2022-07-24 21:25:04 +0700

    2

--- pages/index.js
+++ pages/index.js
@@ -84,6 +84,21 @@ const Home = () => (
           for all things launching products, from planning and designing all the
           way to solving real-life problems with code. Checkout my portfolio to know  more 
           about my work. "you can hire me through my-contact.  "
+          Takuya is a freelance and a full-stack developer based in Osaka with a
+          passion for building digital services/stuff he wants. He has a knack
+          for all things launching products, from planning and designing all the
+          way to solving real-life problems with code. When not online, he loves
+          hanging out with his camera. Currently, he is living off of his own
+          product called{' '}
+          <NextLink href="/works/inkdrop" passHref scroll={false}>
+            <Link>Inkdrop</Link>
+          </NextLink>
+          . He publishes content for marketing his products and his YouTube
+          channel called &quot;
+          <NextLink href="https://www.youtube.com/devaslife" passHref>
+            <Link target="_blank">Dev as Life</Link>
+          </NextLink>
+          &quot; has more than 100k subscribers.
         </Paragraph>
         <Box align="center" my={4}>
           <NextLink href="/works" passHref scroll={false}>
